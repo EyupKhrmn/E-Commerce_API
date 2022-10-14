15b2315b37e3a376a19748012d43256db1708123
EyupKhrmn <eyupkhrmn45@gmail.com>
2022-10-14 17:13:43 +0300

    bir önceki committe atmayı unuttuğm dosyalar

---
+++ Infrastructure/ETicaretAPI.Persistance/Migrations/20221004154343_AddCustomerIDColumForOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETicaretAPI.Persistance.Migrations
+{
+    public partial class AddCustomerIDColumForOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CustometId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustometId",
+                table: "Orders");
+        }
+    }
+}
